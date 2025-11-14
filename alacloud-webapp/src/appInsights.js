947985f2988ee04fc382f5118738186f4ba572f6
Ala Laribi <alaa.laaribi.ext@up.coop>
2025-11-14 18:23:09 +0100

    add-insights

---
+++ alacloud-webapp/src/appInsights.js
@@ -0,0 +1,14 @@
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+
+const appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: import.meta.env.VITE_APP_INSIGHTS_KEY, // Vite style
+    // or if using CRA: process.env.REACT_APP_APP_INSIGHTS_KEY
+    enableAutoRouteTracking: true, // Tracks page changes automatically
+  },
+});
+
+console.log("Insights key:", import.meta.env.VITE_APP_INSIGHTS_KEY);
+
+appInsights.loadAppInsights();
+export default appInsights;
